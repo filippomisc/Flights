59cf32fbf15c9096671ecf9b05b01ade1ddffe57
JIMMY <jimmy@MacBook-Pro-di-JIMMY.local>
2018-05-15 17:45:39 +0200

    Soluzione prima ora

--- Flight/src/it/polito/tdp/flight/db/FlightDAO.java
+++ Flight/src/it/polito/tdp/flight/db/FlightDAO.java
@@ -8,12 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.flight.model.Airline;
+import it.polito.tdp.flight.model.AirlineIdMap;
 import it.polito.tdp.flight.model.Airport;
+import it.polito.tdp.flight.model.AirportIdMap;
 import it.polito.tdp.flight.model.Route;
+import it.polito.tdp.flight.model.RouteIdMap;
 
 public class FlightDAO {
 
-	public List<Airline> getAllAirlines() {
+	public List<Airline> getAllAirlines(AirlineIdMap airlineIdMap) {
 		String sql = "SELECT * FROM airline";
 		List<Airline> list = new ArrayList<>();
 		try {
@@ -22,9 +25,10 @@ public class FlightDAO {
 			ResultSet res = st.executeQuery();
 
 			while (res.next()) {
-				list.add(new Airline(res.getInt("Airline_ID"), res.getString("Name"), res.getString("Alias"),
+				Airline airline = new Airline(res.getInt("Airline_ID"), res.getString("Name"), res.getString("Alias"),
 						res.getString("IATA"), res.getString("ICAO"), res.getString("Callsign"),
-						res.getString("Country"), res.getString("Active")));
+						res.getString("Country"), res.getString("Active"));
+				list.add(airlineIdMap.get(airline));
 			}
 			conn.close();
 			return list;
@@ -35,20 +39,23 @@ public class FlightDAO {
 		}
 	}
 
-	public List<Route> getAllRoutes() {
+	public List<Route> getAllRoutes(AirlineIdMap airlineIdMap, AirportIdMap airportIdMap, RouteIdMap routeIdMap) {
 		String sql = "SELECT * FROM route";
 		List<Route> list = new ArrayList<>();
 		try {
 			Connection conn = ConnectDB.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
-
+			
+			int counter = 0;
 			while (res.next()) {
-				list.add(new Route(res.getString("Airline"), res.getInt("Airline_ID"), res.getString("Source_airport"),
+				Route route = new Route(counter, res.getString("Airline"), res.getInt("Airline_ID"), res.getString("Source_airport"),
 						res.getInt("Source_airport_ID"), res.getString("Destination_airport"),
 						res.getInt("Destination_airport_ID"), res.getString("Codeshare"), res.getInt("Stops"),
-						res.getString("Equipment")));
-			}
+						res.getString("Equipment")); 
+				list.add(routeIdMap.get(route));
+				counter++;
+			}	
 			conn.close();
 			return list;
 		} catch (SQLException e) {
@@ -57,7 +64,7 @@ public class FlightDAO {
 		}
 	}
 
-	public List<Airport> getAllAirports() {
+	public List<Airport> getAllAirports(AirportIdMap airportIdMap) {
 		String sql = "SELECT * FROM airport";
 		List<Airport> list = new ArrayList<>();
 		try {
@@ -66,10 +73,11 @@ public class FlightDAO {
 			ResultSet res = st.executeQuery();
 
 			while (res.next()) {
-				list.add(new Airport(res.getInt("Airport_ID"), res.getString("name"), res.getString("city"),
+				Airport airport = new Airport(res.getInt("Airport_ID"), res.getString("name"), res.getString("city"),
 						res.getString("country"), res.getString("IATA_FAA"), res.getString("ICAO"),
 						res.getDouble("Latitude"), res.getDouble("Longitude"), res.getFloat("timezone"),
-						res.getString("dst"), res.getString("tz")));
+						res.getString("dst"), res.getString("tz")); 
+				list.add(airportIdMap.get(airport));
 			}
 			conn.close();
 			return list;
--- Flight/src/it/polito/tdp/flight/model/Model.java
+++ Flight/src/it/polito/tdp/flight/model/Model.java
@@ -1,5 +1,40 @@
 package it.polito.tdp.flight.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polito.tdp.flight.db.FlightDAO;
+
 public class Model {
+	
+	FlightDAO fdao = null;
+	List<Airport> airports;
+	List<Airline> airlines;
+	List<Route> routes;
+	
+	AirlineIdMap airlineIdMap;
+	AirportIdMap airportIdMap;
+	RouteIdMap routeIdMap;
+	
+	
+	public Model() {
+		fdao = new FlightDAO();
+		
+		airlineIdMap = new AirlineIdMap();
+		airportIdMap = new AirportIdMap();
+		routeIdMap = new RouteIdMap();
+		
+		airlines = fdao.getAllAirlines(airlineIdMap);
+		System.out.println(airlines);
+
+		airports = fdao.getAllAirports(airportIdMap);
+		System.out.println(airports);
+
+		routes = fdao.getAllRoutes(airlineIdMap, airportIdMap, routeIdMap);
+		System.out.println(routes);
+	
+		
+	
+	}
 
 }
